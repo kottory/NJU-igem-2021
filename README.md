375c4d002f56db784ec0cc42404c04d4661e97f7
kottory <kottory@outlook.com>
2021-05-29 16:08:59 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # NJU-igem-2021
+
+## warnings
+- all assets should be uploaded manually due to igemwiki-api.
--- gulpfile.js
+++ gulpfile.js
@@ -8,22 +8,26 @@ const autoprefixer = require('autoprefixer');
 const browserSync = require('browser-sync');
 const { reload } = require('browser-sync');
 
-function build_pug() {
+function make_pug() {
   return src('src/*.pug')
     .pipe(pug())
     .pipe(dest('build/'));
 }
 
-function build_postcss() {
+function make_postcss() {
   const postcssPlugins = [autoprefixer, tailwindcss]
   return src('src/style/*.css').pipe(postcss(postcssPlugins)).pipe(dest('build/style'))
 }
 
-function build_clean() {
+function make_clean() {
   return src('build/', { read: false }).pipe(clean({ force: true }))
 }
 
-const build = series(build_pug, build_postcss);
+function make_image() {
+  return src('src/image/*').pipe(dest('build/image/'));
+}
+
+const make = series(make_pug, make_postcss, make_image);
 
 function browser_sync() {
   browserSync.init({
@@ -33,10 +37,9 @@ function browser_sync() {
     },
     port: 9876
   });
-  gulp.watch('src/*').on("change", series(build, reload));
+  gulp.watch('src/*').on("change", series(make, reload));
 }
 
-
-exports.build = build;
-exports.clean = build_clean;
-exports.server = series(build, browser_sync);
+exports.make = make;
+exports.clean = make_clean;
+exports.server = series(make, browser_sync);
